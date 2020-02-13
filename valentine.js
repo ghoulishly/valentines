b9b5107d754ba3d50d076b046424fa7e14cee88a
Samu <47273262+ghoulishly@users.noreply.github.com>
2020-02-14 00:55:31 +0200

    Add files via upload

---
+++ valentine.js
@@ -0,0 +1,12 @@
+var string = "Hey there sexy... will you be my Valentine? ;)";
+var str = string.split("");
+var el = document.getElementById('str');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
+
+
+
+
+
